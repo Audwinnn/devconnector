977ceb1e0a58e0f1e6c8169b6528375368d342e3
Audwinnn <audouin0033@gmail.com>
2019-11-29 15:10:19 +0800

    User Registration

--- config/db.js
+++ config/db.js
@@ -4,8 +4,9 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-      await mongoose.connect(db, {
-        useNewUrlParser: true
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true
     });
 
     console.log('MongoDB Connected...');
