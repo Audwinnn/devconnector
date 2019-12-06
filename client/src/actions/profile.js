2a6d252aae536cb070e447fb8601e41de215d264
Audwinnn <audouin0033@gmail.com>
2019-12-06 14:54:57 +0800

    Starting On The Profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -46,10 +46,10 @@ export const getProfiles = () => async dispatch => {
   }
 };
 
-// Get profiles by ID
-export const getProfileByID = userId => async dispatch => {
+// Get profile by ID
+export const getProfileById = userId => async dispatch => {
   try {
-    const res = await axios.get(`/api/profile/${userId}`);
+    const res = await axios.get(`/api/profile/user/${userId}`);
 
     dispatch({
       type: GET_PROFILE,
